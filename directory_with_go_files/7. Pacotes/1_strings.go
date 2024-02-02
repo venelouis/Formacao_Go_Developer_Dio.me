253b47d36dd424016438a177cb2956228169adcf
Venelouis <yaoul@live.com>
2024-02-02 13:44:20 -0300

    inciando pacotes

---
+++ directory_with_go_files/7. Pacotes/1_strings.go
@@ -0,0 +1,15 @@
+// pacotes (caixinha de funções prontas para serem usadas)
+// função contains
+// contains: procura dentro de uma string se existe uma substring
+// ex: Terra - rr (true)
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Contains("Computador", "dor"))
+}
