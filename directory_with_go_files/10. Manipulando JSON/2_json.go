b16903952df71c9b2e7980050697a89ea61abb59
Venelouis <yaoul@live.com>
2024-02-13 14:16:35 -0300

    adicionando comentários

--- directory_with_go_files/10. Manipulando JSON/2_json.go
+++ directory_with_go_files/10. Manipulando JSON/2_json.go
@@ -1,51 +1,51 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"strconv"
+	"encoding/json" // Importando a biblioteca para manipular JSON
+	"fmt"           // Importando a biblioteca para imprimir na tela
+	"io/ioutil"     // Importando a biblioteca para ler arquivos
+	"os"            // Importando a biblioteca para manipular arquivos
+	"strconv"       // Importando a biblioteca para converter tipos de dados
 )
 
-type Users struct {
-	Users []User `json:"usuarios"`
+type Users struct { // Criando uma estrutura para armazenar os usuários
+	Users []User `json:"usuarios"` // Criando um array de usuários
 }
 
-type User struct {
-	Nome   string `json:"Nome"`
-	Tipo   string `json:"Tipo"`
-	Idade  int    `json:"Idade"`
-	social Social `json:"rede social"`
+type User struct { // Criando uma estrutura para armazenar os dados do usuário
+	Nome   string `json:"Nome"`        // Criando um campo para armazenar o nome do usuário
+	Tipo   string `json:"Tipo"`        // Criando um campo para armazenar o tipo do usuário
+	Idade  int    `json:"Idade"`       // Criando um campo para armazenar a idade do usuário
+	social Social `json:"rede social"` // Criando um campo para armazenar a rede social do usuário
 }
 
-type Social struct {
-	facebook string `json:"facebook"`
-	twitter  string `json:"twitter"`
+type Social struct { // Criando uma estrutura para armazenar os dados da rede social do usuário
+	facebook string `json:"facebook"` // Criando um campo para armazenar o facebook do usuário
+	twitter  string `json:"twitter"`  // Criando um campo para armazenar o twitter do usuário
 }
 
-func main() {
+func main() { // Função principal
 
-	jsonFile, err := os.Open("usuarios.json")
+	jsonFile, err := os.Open("usuarios.json") // Abrindo o arquivo usuarios.json
 
-	if err != nil {
-		fmt.Println(err)
+	if err != nil { // Verificando se houve erro ao abrir o arquivo
+		fmt.Println(err) // Imprimindo o erro
 	}
 
-	fmt.Println("Arquivo aberto com sucesso")
+	fmt.Println("Arquivo aberto com sucesso") // Imprimindo que o arquivo foi aberto com sucesso
 
-	defer jsonFile.Close()
+	defer jsonFile.Close() // Fechando o arquivo após a execução da função
 
-	byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile) // Lendo o arquivo
 
-	var usuarios Users
+	var usuarios Users // Criando uma variável para armazenar os usuários
 
-	json.Unmarshal(byteValue, &usuarios)
+	json.Unmarshal(byteValue, &usuarios) // Convertendo o JSON para a estrutura criada
 
-	for i := 0; i < len(usuarios.Users); i++ {
-		fmt.Println("Usuario Tipo: " + usuarios.Users[i].Tipo)
-		fmt.Println("Usuário Idade: " + strconv.Itoa(usuarios.Users[i].Idade))
-		fmt.Println("Usuário Nome: " + usuarios.Users[i].Nome)
+	for i := 0; i < len(usuarios.Users); i++ { // Percorrendo o array de usuários
+		fmt.Println("Usuario Tipo: " + usuarios.Users[i].Tipo)                 // Imprimindo o tipo do usuário
+		fmt.Println("Usuário Idade: " + strconv.Itoa(usuarios.Users[i].Idade)) // Imprimindo a idade do usuário
+		fmt.Println("Usuário Nome: " + usuarios.Users[i].Nome)                 // Imprimindo o nome do usuário
 
 	}
 
